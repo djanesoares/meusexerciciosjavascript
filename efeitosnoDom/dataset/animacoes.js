773f334941b98da2d1b73ae61312a591330874f6
djanesoares <djanesoares@gmail.com>
2024-03-21 11:13:19 -0300

    atualizações

--- efeitosnoDom/dataset/animacoes.js
+++ efeitosnoDom/dataset/animacoes.js
@@ -2,19 +2,19 @@ function initTabNav() {
   const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
   const tabContent = document.querySelectorAll('[data-tab="content"] section');
 
-  if(tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('ativo');
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
 
     function activeTab(index) {
       tabContent.forEach((section) => {
-        section.classList.remove('ativo');
+        section.classList.remove("ativo");
       });
       const direcao = tabContent[index].dataset.anime;
-      tabContent[index].classList.add('ativo', direcao);
+      tabContent[index].classList.add("ativo",direcao);
     }
 
     tabMenu.forEach((itemMenu, index) => {
-      itemMenu.addEventListener('click', () => {
+      itemMenu.addEventListener("click", () => {
         activeTab(index);
       });
     });
@@ -23,10 +23,12 @@ function initTabNav() {
 initTabNav();
 
 function initAccordion() {
-  const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
-  const activeClass = 'ativo';
-  
-  if(accordionList.length) {
+  const accordionList = document.querySelectorAll(
+    '[data-anime="accordion"] dt'
+  );
+  const activeClass = "ativo";
+
+  if (accordionList.length) {
     accordionList[0].classList.add(activeClass);
     accordionList[0].nextElementSibling.classList.add(activeClass);
 
@@ -36,22 +38,24 @@ function initAccordion() {
     }
 
     accordionList.forEach((item) => {
-      item.addEventListener('click', activeAccordion);
+      item.addEventListener("click", activeAccordion);
     });
   }
 }
 initAccordion();
 
 function initScrollSuave() {
-  const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
+  const linksInternos = document.querySelectorAll(
+    '[data-menu="suave"] a[href^="#"]'
+  );
 
   function scrollToSection(event) {
     event.preventDefault();
-    const href = event.currentTarget.getAttribute('href');
+    const href = event.currentTarget.getAttribute("href");
     const section = document.querySelector(href);
     section.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
+      behavior: "smooth",
+      block: "start",
     });
 
     // forma alternativa
@@ -63,30 +67,28 @@ function initScrollSuave() {
   }
 
   linksInternos.forEach((link) => {
-    link.addEventListener('click', scrollToSection);
+    link.addEventListener("click", scrollToSection);
   });
 }
 initScrollSuave();
 
 function initAnimacaoScroll() {
   const sections = document.querySelectorAll('[data-anime="scroll"]');
-  if(sections.length) {
+  if (sections.length) {
     const windowMetade = window.innerHeight * 0.6;
 
     function animaScroll() {
       sections.forEach((section) => {
         const sectionTop = section.getBoundingClientRect().top;
-        const isSectionVisible = (sectionTop - windowMetade) < 0;
-        if(isSectionVisible)
-          section.classList.add('ativo');
-        else 
-          section.classList.remove('ativo');
-      })
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) section.classList.add("ativo");
+        else section.classList.remove("ativo");
+      });
     }
 
     animaScroll();
 
-    window.addEventListener('scroll', animaScroll);
+    window.addEventListener("scroll", animaScroll);
   }
 }
 initAnimacaoScroll();
