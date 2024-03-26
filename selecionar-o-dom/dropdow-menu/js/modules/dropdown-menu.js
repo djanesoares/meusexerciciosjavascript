9836beff85969d2f62eb6e5d9de009b36c22189a
djanesoares <djanesoares@gmail.com>
2024-03-26 18:40:20 -0300

    finalizado menu dropdown

--- selecionar-o-dom/dropdow-menu/js/modules/dropdown-menu.js
+++ selecionar-o-dom/dropdow-menu/js/modules/dropdown-menu.js
@@ -1,14 +1,22 @@
-export default function initDropdownMenu() {}
+import outsideClick from "./outsideclick.js";
 
-const dropdownMenus = document.querySelectorAll('[data-dropdown]');
 
-dropdownMenus.forEach(menu => {
-  ["touchstart", "click"].forEach(userEvent => {
-    menu.addEventListener(userEvent,handleClick);
-  })
-})
+export default function initDropdownMenu() {
+  const dropdownMenus = document.querySelectorAll("[data-dropdown]");
+
+dropdownMenus.forEach((menu) => {
+  ["touchstart", "click"].forEach((userEvent) => {
+    menu.addEventListener(userEvent, handleClick);
+  });
+});
 
 function handleClick(event) {
   event.preventDefault();
-  this.classList.toggle('active');
+  this.classList.add("active");
+  outsideClick(this, ["touchstart", "click"], () => {
+    this.classList.remove("active");
+  });
 }
+
+}
+
